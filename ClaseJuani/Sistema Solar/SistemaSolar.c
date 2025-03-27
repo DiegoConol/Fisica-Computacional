378062afd5c104706fe8dd0180615f6cdfdb7b8c
DiegoConol <diegoconolinsti@gmail.com>
2025-03-27 09:52:26 +0100

    Ahora sí, fini

---
+++ ClaseJuani/Sistema Solar/SistemaSolar.c
@@ -0,0 +1,1 @@
+prompt= Give me a program in python which takes an NxN matrix and a N vector and multiplies it, resulting in a N vector. The program should be able to handle any size of matrix and vector.
